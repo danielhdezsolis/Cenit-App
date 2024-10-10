cb6615003dc18f4ff1a1e1c1724f0d897485d706
Daniel_Hernandez <daniel.hernandez@emeraldigital.com>
2024-10-10 13:50:15 -0600

    added ToolBar

--- app/src/main/java/com/example/cenit/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/cenit/ui/home/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cenit.ui.home
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -26,5 +27,26 @@ class MainActivity : AppCompatActivity() {
         val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHost.navController
         binding.bottomNavView.setupWithNavController(navController)
+
+        // Llama a la función que se encarga de actualizar el título
+        setupToolbarTitleChange(binding, navController)
+
+    }
+
+    private fun setupToolbarTitleChange(binding: ActivityMainBinding, navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            // Usar binding para acceder al TextView en el FrameLayout de la toolbar
+            binding.toolbarTitle.apply {
+                text = when (destination.id) {
+                    R.id.runningProofsFragment -> getString(R.string.RunningProofs_Header)
+                    R.id.certificatesFragment -> getString(R.string.Certificates_Header)
+                    R.id.devicesFragment -> getString(R.string.Devices_Header)
+                    R.id.notificationsFragment -> getString(R.string.Notifications_Header)
+                    R.id.accountFragment -> getString(R.string.Account_Header)
+                    R.id.onBoardingFragment -> getString(R.string.OnBoarding_Header)
+                    else -> getString(R.string.app_name) // Texto por defecto
+                }
+            }
+        }
     }
 }
